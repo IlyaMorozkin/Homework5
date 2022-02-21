89e5843664ae215e11bc0ce3c0470c88c6085ec7
Ilya_Morozkin <morozkin.ilya86@mail.ru>
2022-02-21 09:41:54 +0300

    task38

---
+++ task38/Program.cs
@@ -0,0 +1,26 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+
+int[] arr = new int[20];
+int sum = 0;
+int count =1;
+void FillArray()
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 20);
+        Console.Write($"{arr[i]} ");
+    }
+}
+void PrintSum()
+{
+    while (count <= arr.Length)
+    {
+        sum+= arr[count];
+        count = count +2;
+    }
+    Console.Write($"\nСумма чисел на не четных позициях = {sum}");
+}
+
+Console.WriteLine("Сгенерированный массив: ");
+FillArray();
+PrintSum();
